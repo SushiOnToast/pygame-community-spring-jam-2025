04b2f1fbbf1e63264c6683682446a029bf4c0e29
SushiOnToast <sushbomm@gmail.com>
2025-04-27 22:22:39 +0530

    update player.py and tile.py (minor fix)

--- code/player.py
+++ code/player.py
@@ -6,7 +6,7 @@ class Player(pygame.sprite.Sprite):
   def __init__(self, pos, groups, obstacle_sprites):
     super().__init__(groups)
 
-    self.image = pygame.image.load("graphics/test/player.png").convert_alpha()
+    self.image = pygame.image.load("../graphics/test/player.png").convert_alpha()
     self.rect = self.image.get_rect(topleft=pos)
 
     # movement
