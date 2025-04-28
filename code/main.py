1085eb50c7c697730b21ea0859167b8b17620690
SushiOnToast <sushbomm@gmail.com>
2025-04-28 07:03:33 +0530

    fix graphics imports

--- code/main.py
+++ code/main.py
@@ -11,14 +11,14 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame Spring Gamejam")
 
 # Font
-font = pygame.font.Font('graphics/font/Minecraftia-Regular.ttf', 50)
+font = pygame.font.Font('../graphics/font/Minecraftia-Regular.ttf', 50)
 
 # Colors
 TEXT_COL = (255, 255, 255)
 
 # Load button images
-start_img = pygame.image.load("graphics/button/start_btn.png").convert_alpha()
-exit_img = pygame.image.load("graphics/button/exit_btn.png").convert_alpha()
+start_img = pygame.image.load("../graphics/button/start_btn.png").convert_alpha()
+exit_img = pygame.image.load("../graphics/button/exit_btn.png").convert_alpha()
 
 # Create button instances
 start_button = button.Button(456, 210, start_img, 1.3)
--- code/settings.py
+++ code/settings.py
@@ -14,7 +14,7 @@ BG_COLOR = "#e8cfa6"
 COLORKEY = "#00ff00"
 
 # debug
-TESTING_OVERLAY = False
+TESTING_OVERLAY = True
 
 WORLD_MAP = [
     [
